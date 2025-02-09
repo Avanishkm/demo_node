bd3c4657019fa5926fd30e2277aceea2edb7374c
Avanish kumar <avanish.kumar@attplgroup.com>
2025-02-09 13:33:11 +0530

    demo apis

---
+++ index.js
@@ -0,0 +1,16 @@
+const express  = require("express")
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send("Landing Page");
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send("About Page");
+})
+
+const port = 8000;
+app.listen(port, () => {
+    console.log(`Site is running on port ${port}`)
+})
